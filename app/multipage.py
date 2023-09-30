7ec0e387602d314bc68ba0c9fefee1b7f57e6e24
Guspan Tanadi <36249910+guspan-tanadi@users.noreply.github.com>
2023-09-30 14:24:18 +0700

    docs: bit misspell one comment multipage

--- app/multipage.py
+++ app/multipage.py
@@ -32,7 +32,7 @@ class MultiPage:
         self.pages.append({"title": title, "function": func})
 
     def run(self):
-        # Drodown to select the page to run
+        # Dropdown to select the page to run
         page = st.sidebar.selectbox(
             "Navigation", self.pages, format_func=lambda page: page["title"]
         )
